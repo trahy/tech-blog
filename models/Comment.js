80f02ac54da6513527e7573f79c33ea0b7eadcdc
unknown <trahy032@gmail.com>
2023-09-30 23:40:13 +0930

    updated models

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,50 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
 Comment.init(
   {
-    body: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
+      id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+      },
+
+      comment_text: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: { 
+              len: [3] //min 3 character for comment
+          },
+      },
+
+      user_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+              model: "user",
+              key: "id",
+          },
+      },
+
+      comment_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+              model: "article",
+              key: "id",
+          },
+      },
   },
   {
-    sequelize
-  }
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: "comment",
+  },
 );
 
 module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -2,19 +2,33 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: "cascade"
+});
+
 Post.belongsTo(User, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
+  foreignKey: 'user_id',
+  onDelete: "cascade"
 });
 
 Post.hasMany(Comment, {
-  foreignKey: 'postId',
-  onDelete: 'CASCADE'
+  foreignKey: 'post_id',
+  onDelete: "cascade"
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
+  foreignKey: 'user_id',
+  onDelete: "cascade"
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: "cascade"
 });
 
 module.exports = { User, Post, Comment };
